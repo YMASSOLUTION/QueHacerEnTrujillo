b9f597e254b780ddd37642f7b303e39977f491b0
YMASSOLUTION <ymassolution@gmail.com>
2016-08-11 19:44:48 -0500

    primera entrada

---
+++ Web Site/SITE/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title>¿Que Hacer en Trujillo?</title>
+		<meta name="viewport" content="width=device-width">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+		<link rel="stylesheet" type="text/css" href="css/responsive.css">
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<script src="js/jquery.slides.min.js"></script>
+	</head>
+	<body>
+		<!-- Comienso de Cabecera -->
+		<header>
+			<div id="logo">
+					<p>¿Que Hacer En Trujillo?</p>
+					<span>Eventos y mas...</span>
+			</div>
+			<!--<div id="home" class="top-banner">
+				 <div class="header1">
+					 <div class="container">
+						 <div class="headr-left">
+							 <div class="details">
+								 <ul>
+									 <li><a href="contactenos"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Clínica Rodríguez</a></li>
+									 <li><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>(+044) 66 98 82</li>
+								 </ul>
+							 </div>
+							 <div class="clearfix"></div>
+						 </div>
+						 <div class="headr-right">
+							 <div class="social">							
+									<a href="https://www.facebook.com/Cl%C3%ADnica-Cirug%C3%ADa-Rodriguez-632591650214976/"><i class="facebook"></i></a>
+									<a href="https://twitter.com/CliniRodriguez"><i class="twitter"></i></a>
+									<a href="https://plus.google.com/u/0/114321046278383208604"><i class="google"></i></a>	
+							 </div>				 
+						 </div>
+						 <div class="clearfix"></div>
+					 </div>
+				 </div>
+			</div>-->	
+		</header>
+	
+	
+	
+	</body>
+</html>
